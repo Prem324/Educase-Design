9160f5614f25884c256092ba5f45b5b113bb364e
Prem324 <prem324r@gmail.com>
2025-06-05 13:37:40 +0530

    Develop-Layout

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -5,14 +5,16 @@ import "./index.css";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <h1>Welcome to PopX</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-      <Link to="/signup">
-        <button className="cre-acc-btn">Create Account</button>
-      </Link>
-      <Link to="/login">
-        <button className="ale-reg-btn">Already Registered? Login</button>
-      </Link>
+      <div>
+        <h1>Welcome to PopX</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+        <Link to="/signup">
+          <button className="cre-acc-btn">Create Account</button>
+        </Link>
+        <Link to="/login">
+          <button className="ale-reg-btn">Already Registered? Login</button>
+        </Link>
+      </div>
     </div>
   );
 };
