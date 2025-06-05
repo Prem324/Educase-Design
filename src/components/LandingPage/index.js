dc64a3ea963e88c6913a85604578566ec7e3989e
Prem324 <prem324r@gmail.com>
2025-06-05 11:54:31 +0530

    Develop-Layout

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import "./index.css";
+// import "./index.css";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>LandingPage</h1>
+    <div className="landing-page">
+      <div>
+        <h1>Welcome to PopX</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+        <button>Create Account</button>
+        <button>Already Registered? Login</button>
+      </div>
     </div>
   );
 };
