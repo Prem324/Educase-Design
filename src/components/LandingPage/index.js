5b3fc343e15849f5ea53c286ba710ffa1456cdae
Prem324 <prem324r@gmail.com>
2025-06-05 10:58:11 +0530

    Intialize Project

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1>LandingPage</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
